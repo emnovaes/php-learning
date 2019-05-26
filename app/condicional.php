ea3d9539ed071f763082b9789ff8a77d01cae4c5
edson.novaes <edson.novaes@fuabc.org.br>
2019-05-25 21:14:18 -0300

    git resolvendo conflitos

---
+++ app/condicional.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form method="get" action="CondResposta.php">
+        Ano de Nascimento: 
+        <input type="text" placeholder= "4 digitos" name="ano"/>
+        <input type="submit" value="Verificar"/>
+</body>
+</html>
