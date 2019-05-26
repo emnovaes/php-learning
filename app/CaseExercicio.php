cf0ccd3d08d7dd330423149623eafbce1377d112
edson.novaes <edson.novaes@fuabc.org.br>
2019-05-25 22:10:31 -0300

    Condicional Case

---
+++ app/CaseExercicio.php
@@ -0,0 +1,23 @@
+<?php
+    $n      = $_GET['num'];
+    $oper   = $_GET['oper'];
+
+    
+    switch ($oper) {
+        case 1:
+            $r = $n * 2;
+            break;
+        case 2:
+            $r = $n ^ 2;
+            break;
+        case 3:
+            $r = sqrt($n);
+            break;
+        
+        default:
+            # code...
+            break;
+    }
+    echo "resultado $r"
+
+?>
---
+++ app/ConditionalCase.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="CaseExercicio.php" method="get"></form>
+        Numero: <input type="number" name="num">
+        <fieldset><legend>Operacao</legend>
+            <input type="radio" name="oper" id="dobro" value="1">
+            <label for="dobro">Dobro</label>
+            <input type="radio" name="oper" id="cubo" value="2">
+            <label for="cubo">Cubo</label>
+            <input type="radio" name="oper" id="raiz" value="3">
+            <label for="raiz">Raiz Quadrada</label>
+        </fieldset>
+        <input type="button" value="Calcular"> 
+    </form>
+</body>
+</html>
