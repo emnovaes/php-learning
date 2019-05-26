fc9955780f671afa91e3ae0a303e935e1b426344
edson.novaes <edson.novaes@fuabc.org.br>
2019-05-26 12:45:27 -0300

    modelando novo site

--- index.php
+++ index.php
@@ -1,4 +1,29 @@
-<?php
-    $var = "Hello World! <br> incluindo variavel <br> <br> novo teste com git";
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <link rel="stylesheet" href="css/estilo.css" type="text/css" media="screen">
+    <meta charset="UTF-8">
+    <title>PHP - Intermediario</title>
+</head>
+<body>
+    <header>
+        <h1>Curso PHP Intermediário</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="empresa.php">Empresa</a></li>
+                <li><a href="quemsomos.php">Quem Somos</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <p>Conteudo Index</p>
+    </section>
+    <footer>
+        <p>Todos os direitos reservados AccertSystem</p>
+    </footer>
 
-    echo $var;
+    
+</body>
+</html>
